c19e807a1f9980349b04b4518f8505301dd239ea
FelipeWanner <felipe.w.pakua@gmail.com>
2023-07-19 20:36:03 +1000

    adding banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner () {
+    return(
+        <header className='banner'>
+            <img src="/images/banner.png" alt="Main page banner of Organo."/>
+        </header>
+    )
+}
+
+export default Banner
