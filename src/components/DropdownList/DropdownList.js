45ad22d3d98ce16013594268dbbd0bef97fa6a70
FelipeWanner <felipe.w.pakua@gmail.com>
2023-07-23 20:51:32 +1000

    finalizing the form layout

---
+++ src/components/DropdownList/DropdownList.js
@@ -0,0 +1,16 @@
+import "./DropdownList.css";
+
+const DropdownList = (props) => {
+    return(
+        <div className="dropDown-list">
+            <label>{props.label}</label>
+            <select required={props.mandatory}>
+                {props.items.map(item => <option key={item}>{item}</option>)}
+                {/*O react obriga que cada item de uma lista possua uma 'key', e essa key pode ser qualquer coisa.
+                Nesse caso, decidimos passar o proprio 'item' como key, ja que cada item eh um nome diferente */}
+            </select>
+        </div>
+    )
+}
+
+export default DropdownList;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,36 @@
 import "./Form.css";
 import TextField from "../TextField/TextField"
+import DropdownList from "../DropdownList/DropdownList";
+import Button from "../Button/Button";
 
 const Form = () => {
+
+    const teams = [
+        'programacao',
+        'front end',
+        'datascience',
+        'devops',
+        'ux & design',
+        'mobile',
+        'inovacao e gestao',
+    ]
+
+    const formSubmiting = (event) => {
+        console.log("oi")
+        event.preventDefault();
+    }
+
     return(
         <section className="myForm">
-            <form>
+            <form onSubmit={formSubmiting}>
                 <h2>Fill in the information to create the employee's card</h2>
-                <TextField label="Name" placeholder="Enter Your Name"/>
-                <TextField label="Position"placeholder="Enter Your Position"/>
-                <TextField label="Image"placeholder="Provide the image URL"/>
+                <TextField mandatory={true} label="Name" placeholder="Enter Your Name"/>
+                <TextField mandatory={true} label="Position" placeholder="Enter Your Position"/>
+                <TextField mandatory={false} label="Image" placeholder="Provide the image URL"/>
+                <DropdownList mandatory={true} label="Team" items={teams}/>
+                <Button>
+                    Create Card
+                </Button>
             </form>
         </section>
     );
