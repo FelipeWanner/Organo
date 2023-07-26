0bd00562edf0e148fe39b37e69f60e7656d667c7
FelipeWanner <felipe.w.pakua@gmail.com>
2023-07-26 17:01:08 +1000

    adding condition for Team to only render a team if there is any card in its array

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,8 +2,12 @@ import Employee from "../Employee/Employee";
 import "./Team.css";
 
 const Team = (props) => {
+  
   return (
-    <section className="team" style={{ backgroundColor: props.secondaryColor }}>
+    //essa eh a maneira de se fazer uma condicional em react. Outro caso seria fazer um operador ternario
+    //o react ira avaliar se a primeira parte eh false ou true, caso true, ele continua lendo a parte da direita,
+    //caso false, ele para e nao le o restante do codigo depois de "&&"
+    (props.employees.length > 0) && <section className="team" style={{ backgroundColor: props.secondaryColor }}>
       <h3 style={{ borderColor: props.primaryColor }}>{props.teamName}</h3>
       <div className="team-employee">
         {props.employees.map((employee) => (
